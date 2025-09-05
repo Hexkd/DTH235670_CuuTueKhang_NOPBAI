97cd04c6a027893a806bb5948cf54b42826f76d0
Hexkd <cuutuekhang@gmail.com>
2025-09-05 10:52:36 +0700

    Upload Chuong3

---
+++ Chuong3/Cau_01.py
@@ -0,0 +1,6 @@
+print("Chương trình kiểm tra năm nhuần\n")
+year = int(input("Nhập vào một năm: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"Năm {year} là năm nhuần")
+else:
+    print(f"Năm {year} không phải là năm nhuần")
